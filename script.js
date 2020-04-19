328c75dfd905d76fbcd9cac7cc4dfefd61868f68
Kelly Cox <thekellycox@gmail.com>
2020-04-19 17:02:27 -0400

    completed javascript framework for functionality. Next step styling

--- script.js
+++ script.js
@@ -4,34 +4,109 @@ let contentBox = document.getElementById('displayContent');
 let cycleSlider = document.getElementById('cycleSlider');
 let continueButton = document.getElementById('continueButton');
 let duration = 0;
-
+let breakTime = 0;
 let timerDisplay = document.createElement('span')
+let cycleCounter = 0;
+let cycleCounterDisplay = document.createElement('span');
+cycleCounterDisplay.setAttribute('id', 'cycleCounter')
+let stopButton = document.createElement('button')
+stopButton.setAttribute('id', 'stopButton')
+stopButton.innerHTML = 'stop'
 
+let pauseButton = document.createElement('button')
+pauseButton.setAttribute('id', 'stopButton')
+pauseButton.innerHTML = 'pause'
+let isPaused = false;
+
+let taskCounter = 0;
+let taskBoard = document.createElement('div');
+taskBoard.setAttribute('id', 'taskBoard');
 
 function updateDisplay(){
   contentBox.innerHTML = '';
   displayTitle.innerHTML = '';
-  contentBox.appendChild(timerDisplay)
+  contentBox.appendChild(timerDisplay);
+  contentBox.appendChild(cycleCounterDisplay);
+  contentBox.appendChild(stopButton);
+  contentBox.appendChild(pauseButton);
+  
+  stopButton.addEventListener('click', ()=>{
+    timerDisplay.innerHTML = '';
+  });
+
+function displayResults() {
+  let resultBox = document.createElement('div')
+  let refreshButton = document.createElement('a');
+
+  refreshButton.innerHTML = 'Restart'
+  refreshButton.setAttribute('id', 'refreshButton')
+  refreshButton.setAttribute('href', 'javascript:history.go(0)')
+  contentBox.innerHTML = '';
+  isPaused = true;
+  
+  resultBox.setAttribute('id', 'resultBox');
+  resultBox.innerHTML = `You have completed ${cycleCounter} cycle(s).`
+
+  contentBox.appendChild(resultBox);
+  taskBoard.innerHTML = `You completed ${taskCounter} task(s)`
+  contentBox.appendChild(taskBoard);
+  contentBox.appendChild(refreshButton);
+  
+
+}
+  stopButton.addEventListener('click', displayResults)
+  pauseButton.addEventListener('click', ()=>{
+    if(isPaused == false){
+      isPaused = true;
+      pauseButton.innerHTML = 'Resume'
+      console.log('paused')
+    } else {
+      isPaused = false;
+      pauseButton.innerHTML = 'Pause'
+      console.log('unpaused')
+    }
+  })
 }
 
-function setUpTimer(){
-  duration *= 1;
-  timerDisplay.innerHTML = `${parseInt(duration /60, 10)}:${parseInt(duration % 60, 10)}`;
+function setUpWorkTimer(limit){
+  limit *= 1;
+  timerDisplay.innerHTML = `${parseInt(limit /60, 10)}:${parseInt(limit % 60, 10)}`;
   let countdown = setInterval(()=>{
-    timerDisplay.innerHTML = `${parseInt(duration /60, 10)}:${parseInt(duration % 60, 10)}`;
-    if(duration > 0){
-    duration--
-    console.log(duration)
+    timerDisplay.innerHTML = `${parseInt(limit /60, 10)}:${parseInt(limit % 60, 10)}`;
+    if(isPaused == true) { return
+    }
+    if(limit > 0){
+    limit--
+    console.log(limit)
     }else {
-      console.log('finished')
+      displayTitle.innerText= 'Break Time'
       clearInterval(countdown);
+      setUpBreakTimer(breakTime);
+      cycleCounter++;
+      cycleCounterDisplay.innerHTML = `${cycleCounter}`
     }
   }, 1000)
   
-  console.log(duration)
+  console.log(limit)
 }
  
-
+function setUpBreakTimer(breakLimit){
+  breakLimit *= 1;
+  timerDisplay.innerHTML = `${parseInt(breakLimit /60, 10)}:${parseInt(breakLimit % 60, 10)}`;
+  let countdown = setInterval(()=>{
+    timerDisplay.innerHTML = `${parseInt(breakLimit /60, 10)}:${parseInt(breakLimit % 60, 10)}`;
+    if(breakLimit > 0){
+      breakLimit--
+    console.log(breakLimit)
+    }else {
+      displayTitle.innerText= 'Back To Work!'
+      clearInterval(countdown);
+      setUpWorkTimer(duration);
+    }
+  }, 1000)
+  
+  console.log(breakLimit)
+}
 
 
 function setduration(){
@@ -39,12 +114,15 @@ function setduration(){
   switch(cycleSlider.value){
     case '1':
       duration = 15;
+      breakTime = 5;
       break;
     case '2':
       duration = 25;
+      breakTime = 5;
       break;
     case '3':
       duration = 45;
+      breakTime = 10;
       break;
     default:
       break;;
@@ -53,14 +131,38 @@ function setduration(){
 }
 
 function noTasks() {
-  setUpTimer();
+  setUpWorkTimer(duration);
   updateDisplay();
-  console.log(`no ${duration}`)
-
-
+  
 }
 function tasks() {
   console.log('yes')
+  updateDisplay();
+  let addButton = document.createElement('button')
+  addButton.innerHTML = '+';
+  let subButton = document.createElement('button')
+  subButton.innerHTML = '-';
+  let taskDisplay = document.createElement('span')
+  taskDisplay.setAttribute('id', 'taskDisplay');
+
+  taskDisplay.innerText = `${taskCounter}`
+  taskBoard.appendChild(taskDisplay);
+  taskBoard.appendChild(addButton);
+  taskBoard.appendChild(subButton);
+
+  addButton.addEventListener('click', ()=> {
+    taskCounter++
+    taskDisplay.innerText = `${taskCounter}`
+  })
+  subButton.addEventListener('click', ()=>{
+    taskCounter--
+    taskDisplay.innerText = `${taskCounter}`
+  })
+
+  contentBox.appendChild(taskBoard)
+  setUpWorkTimer(duration);
+  
+
 }
 function counterOption(){
   setduration()
