213e90382fac3631dd1b794052a665fea3510e8d
Kelly Cox <thekellycox@gmail.com>
2020-04-18 21:43:54 -0400

    completed frame work for site. Completed first timer for cycle, need to create timer for breaks and counter for cycles completed.

--- script.js
+++ script.js
@@ -3,25 +3,59 @@ let displayTitle = document.getElementById('displayTitle');
 let contentBox = document.getElementById('displayContent');
 let cycleSlider = document.getElementById('cycleSlider');
 let continueButton = document.getElementById('continueButton');
-let timer = document.createElement('div')
-let timeValue = 0;
+let duration = 0;
 
-function setUpTimer(){
-  let target = timeValue * 60;
-  console.log(target)
+let timerDisplay = document.createElement('span')
 
-  
-  let countdown = setInterval((target)=>{
-    target--
-    console.log(target)
-  }, 1000);
 
+function updateDisplay(){
+  contentBox.innerHTML = '';
+  displayTitle.innerHTML = '';
+  contentBox.appendChild(timerDisplay)
 }
 
+function setUpTimer(){
+  duration *= 1;
+  timerDisplay.innerHTML = `${parseInt(duration /60, 10)}:${parseInt(duration % 60, 10)}`;
+  let countdown = setInterval(()=>{
+    timerDisplay.innerHTML = `${parseInt(duration /60, 10)}:${parseInt(duration % 60, 10)}`;
+    if(duration > 0){
+    duration--
+    console.log(duration)
+    }else {
+      console.log('finished')
+      clearInterval(countdown);
+    }
+  }, 1000)
+  
+  console.log(duration)
+}
+ 
+
+
+
+function setduration(){
+  
+  switch(cycleSlider.value){
+    case '1':
+      duration = 15;
+      break;
+    case '2':
+      duration = 25;
+      break;
+    case '3':
+      duration = 45;
+      break;
+    default:
+      break;;
+  }
+  console.log(duration);
+}
 
 function noTasks() {
   setUpTimer();
-  console.log(`no ${timeValue}`)
+  updateDisplay();
+  console.log(`no ${duration}`)
 
 
 }
@@ -29,7 +63,8 @@ function tasks() {
   console.log('yes')
 }
 function counterOption(){
-  console.log(timeValue);
+  setduration()
+  console.log(duration);
   displayTitle.innerHTML = '<h2>Do you need a task-counter?</h2>';
   contentBox.innerHTML = '';
   let yesButton = document.createElement('button');
@@ -46,22 +81,9 @@ function counterOption(){
 }
 
 
-
+/*
 cycleSlider.oninput = () =>{
-  switch(cycleSlider.value){
-    case '1':
-      timeValue = 15;
-      break;
-    case '2':
-      timeValue = 25;
-      break;
-    case '3':
-      timeValue = 45;
-      break;
-    default:
-      break;;
-  }
-  console.log(timeValue);
+  setduration()
 }
-
+*/
 continueButton.addEventListener('click', counterOption);
